e1ec15f7ad139d0d9e16d4df5fe4d3dd5dd7b37e
HJ <164671672+HJayBTDH27@users.noreply.github.com>
2024-10-30 13:02:25 -0400

    Score counting rules complete

--- yatzyEngine.js
+++ yatzyEngine.js
@@ -70,28 +70,93 @@ function subtotalAndBonus ( array ) {
     return sum, bonus;
 }
 
-function onePair() {
-    // some code
+function onePairTwoPair( array ) {
+    const numberCount = {};
+    let sum = 0;
+
+    for (let number of array) {
+        numberCount[number] = (numberCount[number] || 0) + 1;
+    }
+
+    for (let [number, count] of Object.entries(numberCount)) {
+        if (count >= 2) {
+            sum += Number(number) * 2;
+        }
+    }
+
+    return sum;
 }
 
-function twoPair() {
-    // some code
+function threeOfAKind( array ) {
+    const numberCount = {};
+    let sum = 0;
+
+    for (let number of array) {
+        numberCount[number] = (numberCount[number] || 0) + 1;
+    }
+
+    for (let [number, count] of Object.entries(numberCount)) {
+        if (count >= 3) {
+            sum += Number(number) * 3;
+        }
+    }
+
+    return sum;
 }
 
-function threeOfAKind() {
-    // some code
+function fourOfAKind( array ) {
+    const numberCount = {};
+    let sum = 0;
+
+    for (let number of array) {
+        numberCount[number] = (numberCount[number] || 0) + 1;
+    }
+
+    for (let [number, count] of Object.entries(numberCount)) {
+        if (count >= 4) {
+            sum += Number(number) * 4;
+        }
+    }
+
+    return sum;
 }
 
-function fourOfAKind() {
-    // some code
+function smallStraight( array ) {
+    array.sort((a, b) => a - b);
+    let consecutiveCount = 1;
+    
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] === array[i - 1] + 1) {
+            consecutiveCount++;
+            if (consecutiveCount === 4) {
+                // TODO: return the sum of the straight
+                return true;  // Found a small straight
+            }
+        } else if (array[i] !== array[i - 1]) {
+            consecutiveCount = 1;  // Reset if not consecutive
+        }
+    }
+    
+    return false;  // No small straight found
 }
 
-function smallStraight() {
-    // some code
-}
-
-function largeStraight() {
-    // some code
+function largeStraight( array ) {
+    array.sort((a, b) => a - b);
+    let consecutiveCount = 1;
+    
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] === array[i - 1] + 1) {
+            consecutiveCount++;
+            if (consecutiveCount === 5) {
+                // TODO: return the sum of the straight
+                return true;  // Found a large straight
+            }
+        } else if (array[i] !== array[i - 1]) {
+            consecutiveCount = 1;  // Reset if not consecutive
+        }
+    }
+    
+    return false;  // No large straight found
 }
 
 function fullHouse() {
@@ -104,8 +169,26 @@ function chance(array) {
     return sum;
 }
 
-function finalScore() {
-    // some code
+function yatzyRoll( array ) {
+    let flag = false;
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] === array[i - 1]) {
+            flag = true;
+        } else {
+            return false;
+        }
+    }
+    if (flag == true) {
+        let total = 0;
+        let sum = array.map(e => total += e);
+        return sum;
+    }
+}
+
+function finalScore( array ) {
+    let total = 0;
+    let sum = array.map(e => total += e);
+    return sum;
 }
 
 
